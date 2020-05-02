b576a78716faa0d4396974b0aac0edbed7249792
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-05-02 13:33:32 -0700

    add responsiveness

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ["../stories/**/*.stories.tsx"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-viewport/register",
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
---
+++ .storybook/preview.js
@@ -0,0 +1,9 @@
+import { addParameters } from "@storybook/react";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { addDecorator } from "@storybook/react";
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS, // newViewports would be an ViewportMap. (see below for examples)
+  },
+});
