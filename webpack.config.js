7dd92bc194b3f2503bbe1f3c76a5475a621810f7
Essential Randomness <essentialrandomn3ss@gmail.com>
2020-05-04 18:15:11 -0700

    make it possible to build for distribution

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
