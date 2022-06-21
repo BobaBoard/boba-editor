64d2da34d47d8b159961c3f62c00206c2b74c882
Essential Randomness <essential.randomn3ss@gmail.com>
2022-06-20 23:24:59 -0700

    upgrade webpack

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,39 +1,32 @@
 const webpackConfig = require("../webpack.config.cjs");
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   stories: ["../stories/**/*.stories.tsx"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-controls",
-    "@storybook/addon-viewport/register",
-    "@storybook/addon-actions",
-    "@storybook/addon-a11y",
-    "storybook-css-modules-preset",
-  ],
-  webpackFinal: async (config) => {
+  addons: ["@storybook/addon-links", "@storybook/addon-controls", "@storybook/addon-viewport/register", "@storybook/addon-actions", "@storybook/addon-a11y", "storybook-css-modules-preset"],
+  webpackFinal: async config => {
     config.devtool = "inline-source-map";
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve("babel-loader"),
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      ],
+      use: [{
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      }]
     });
-
     config.module.rules.push({
       test: /\.html$/,
-      use: ["html-loader"],
+      use: ["html-loader"]
     });
-
     config.resolve.extensions.push(".ts", ".tsx");
     return config;
   },
   features: {
-    postcss: false,
+    postcss: false
   },
+  core: {
+    builder: "webpack5"
+  }
 };
